990591265a3b304e34f4b974ef93f85e125aabc9
Roger Frauca <roger.frauca@gmail.com>
2012-04-25 23:14:41 +0200

    done all

--- Ruby_samples/rails_3_tutorial/sample_app/app/controllers/microposts_controller.rb
+++ Ruby_samples/rails_3_tutorial/sample_app/app/controllers/microposts_controller.rb
@@ -2,6 +2,9 @@ class MicropostsController < ApplicationController
 	before_filter :authenticate, :only => [:create, :destroy] #defined in the SessionHelper
 	before_filter :authorized_user, :only => :destroy
  
+	
+
+
 	def create
 		@micropost = current_user.microposts.build(params[:micropost])
 		if @micropost.save
@@ -18,10 +21,15 @@ class MicropostsController < ApplicationController
 	redirect_back_or root_path
 	end
 	
+	
+
+	
 	private
 		def authorized_user
 		@micropost = Micropost.find(params[:id])
 		redirect_to root_path unless current_user?(@micropost.user)
 		end
+		
+	
 
 end
--- Ruby_samples/rails_3_tutorial/sample_app/app/helpers/users_helper.rb
+++ Ruby_samples/rails_3_tutorial/sample_app/app/helpers/users_helper.rb
@@ -1,5 +1,6 @@
 module UsersHelper
 	def gravatar_for(user, options = { :size => 50 })
+		logger.info(user.inspect)
 		gravatar_image_tag(user.email.downcase, :alt => user.name,
 							:class => 'gravatar',
 							:gravatar => options)
