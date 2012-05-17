fecaab7be79931d8dccd892380db378c9eb902a7
roger.frauca <roger.frauca@gmail.com>
2012-05-17 06:51:54 +0200

    a simple form

--- GoogleAppsEngine/djnorel/books/models.py
+++ GoogleAppsEngine/djnorel/books/models.py
@@ -5,9 +5,9 @@ class Publisher(models.Model):
     name = models.CharField(max_length=30)
     address = models.CharField(max_length=50)
     city = models.CharField(max_length=60)
-    state_province = models.CharField(max_length=30)
-    country = models.CharField(max_length=50)
-    website = models.URLField()
+    state_province = models.CharField(max_length=30,blank=True)
+    country = models.CharField(max_length=50,blank=True)
+    website = models.URLField(blank=True)
     
     def __unicode__(self):
         return self.name
--- GoogleAppsEngine/djnorel/urls.py
+++ GoogleAppsEngine/djnorel/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
 from core.views import hello
+from books import views 
 
 admin.autodiscover()
 handler500 = 'djangotoolbox.errorviews.server_error'
@@ -10,4 +11,6 @@ urlpatterns = patterns('',
     ('^$', 'django.views.generic.simple.direct_to_template',{'template': 'home.html'}),
     ('^hello/$',hello),
     (r'^admin/', include(admin.site.urls)),
+    (r'^search-form/$', views.search_form),
+    (r'^search/$', views.search),
 )
