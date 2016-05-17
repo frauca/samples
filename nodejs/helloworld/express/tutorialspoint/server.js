8bb765062b21c8959b855550ca4ac6e6d92c5e42
Roger frauca <roger.frauca@gmail.com>
2016-05-17 23:48:49 +0200

    now it could post a message UAUUUUU

--- nodejs/helloworld/express/tutorialspoint/server.js
+++ nodejs/helloworld/express/tutorialspoint/server.js
@@ -1,9 +1,50 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+// This responds with "Hello World" on the homepage
 app.get('/', function (req, res) {
-   res.send('Hello World');
+   console.log("Got a GET request for the homepage");
+   res.sendFile( __dirname + "/public/" + "index.htm" );
 })
+
+
+// This responds a POST request for the homepage
+app.post('/', function (req, res) {
+   console.log("Got a POST request for the homepage");
+   // Prepare output in JSON format
+   response = {
+       first_name:req.body.first_name,
+       last_name:req.body.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+
+// This responds a DELETE request for the /del_user page.
+app.delete('/del_user', function (req, res) {
+   console.log("Got a DELETE request for /del_user");
+   res.send('Hello DELETE');
+})
+
+// This responds a GET request for the /list_user page.
+app.get('/list_user', function (req, res) {
+   console.log("Got a GET request for /list_user");
+   res.send('Page Listing');
+})
+
+// This responds a GET request for abcd, abxcd, ab123cd, and so on
+app.get('/ab*cd', function(req, res) {
+   console.log("Got a GET request for /ab*cd");
+   res.send('Page Pattern Match');
+})
+
 
 var server = app.listen(8081, function () {
 
