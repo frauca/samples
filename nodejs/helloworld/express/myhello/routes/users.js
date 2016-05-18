69329aba52eee6ef62ccbf9bdbc6ea24a4f8644f
Roger frauca <roger.frauca@gmail.com>
2016-05-18 23:13:54 +0200

    canvis tontos al exemple generat per el propy express-generator

--- nodejs/helloworld/express/myhello/routes/users.js
+++ nodejs/helloworld/express/myhello/routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// handler for the /user/:id path, which prints the user ID
+router.get('/:id', function (req, res) {
+  res.send(req.params.id);
+});
+
 module.exports = router;
