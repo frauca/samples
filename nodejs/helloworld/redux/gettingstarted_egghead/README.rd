cfc1158406f2524e7ce467ff561e24957608a40f
Roger frauca <roger.frauca@gmail.com>
2016-06-20 23:00:44 +0200

    reducer simple sample

---
+++ nodejs/helloworld/redux/gettingstarted_egghead/README.rd
@@ -0,0 +1,3 @@
+# REDUX getting started
+
+following https://egghead.io/lessons/javascript-redux-describing-state-changes-with-actions
--- nodejs/helloworld/redux/gettingstarted_egghead/scripts/app.jsx
+++ nodejs/helloworld/redux/gettingstarted_egghead/scripts/app.jsx
@@ -1,3 +1,23 @@
-function counter(){
+import expect from 'expect'
 
+function counter(state,action){
+  switch (action.type) {
+    case "INCREMENET":
+      return state+1;
+    case "DECREMENET":
+      return state-1;
+    default:
+      return state;
+  }
+  return state;
 }
+
+expect(
+  counter(0,{type:"INCREMENET"})
+).toEqual(1);
+
+expect(
+  counter(2,{type:"DECREMENET"})
+).toEqual(1);
+
+console.log("Test passed");
