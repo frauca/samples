0d00a4107142f06ba13f236d3e89ab6a4d8737a4
Roger Frauca Cordoba <roger.frauca@gmail.com>
2018-03-26 14:03:46 +0200

    me caguen git

---
+++ python/helloWorld/helloClass/helloInheritance.py
@@ -0,0 +1,27 @@
+class Parent(object):        # define parent class
+   parentAttr = 100
+   def __init__(self):
+      print "Calling parent constructor"
+
+   def parentMethod(self):
+      print 'Calling parent method'
+
+   def setAttr(self, attr):
+      Parent.parentAttr = attr
+
+   def getAttr(self):
+      print "Parent attribute :", Parent.parentAttr
+
+class Child(Parent): # define child class
+   def __init__(self):
+      super(Child, self).__init__()
+      print "Calling child constructor"
+
+   def childMethod(self):
+      print 'Calling child method'
+
+c = Child()          # instance of child
+c.childMethod()      # child calls its method
+c.parentMethod()     # calls parent's method
+c.setAttr(200)       # again call parent's method
+c.getAttr()          # again call parent's method
