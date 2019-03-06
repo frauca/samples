5f37df10cfa24e7adb9c0bacd024c77c25a5aa88
Roger Frauca <roger.frauca@gmail.com>
2019-03-06 21:46:31 +0100

    we are authenticating but all public

--- java/springboot/helloworld/rest1/src/test/java/spring/boot/helloworld/rest/RestApplicationTests.java
+++ java/springboot/helloworld/rest1/src/test/java/spring/boot/helloworld/rest/RestApplicationTests.java
@@ -2,8 +2,12 @@ package spring.boot.helloworld.rest;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,4 +17,5 @@ public class RestApplicationTests {
 	public void contextLoads() {
 	}
 
+	
 }
--- java/springboot/helloworld/rest1/src/test/java/spring/boot/helloworld/rest/unit/rest/GreetingRestTest.java
+++ java/springboot/helloworld/rest1/src/test/java/spring/boot/helloworld/rest/unit/rest/GreetingRestTest.java
@@ -1,20 +1,21 @@
 package spring.boot.helloworld.rest.unit.rest;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import spring.boot.helloworld.rest.model.Greeting;
 import spring.boot.helloworld.rest.rest.GreetingRest;
 
 @RunWith(SpringRunner.class)
@@ -24,9 +25,13 @@ public class GreetingRestTest {
 	@Autowired
     private MockMvc mockMvc;
 	
+	
+	
 	@Test
     public void greetingShouldReturnMessageFromService() throws Exception {
         this.mockMvc.perform(get("/greeting?name=Roger")).andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string(containsString("Hello, Roger")));
     }
+	
+	
 }
