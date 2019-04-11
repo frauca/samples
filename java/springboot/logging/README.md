a57540c7090932d2df48aab09563133232a01e21
roger <roger.frauca@dxcfds.com>
2019-04-11 07:56:22 +0200

    simple projecte to investigate logging

---
+++ java/springboot/logging/README.md
@@ -0,0 +1,5 @@
+#Logging
+
+On one project the logging with java.util.logging is not working properly
+
+I have start this one to analize that fetaure
---
+++ java/springboot/logging/src/main/java/frauca/samples/spring/logging/util/LoggingApplication.java
@@ -0,0 +1,28 @@
+package frauca.samples.spring.logging.util;
+
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * Test the logging
+ */
+@SpringBootApplication
+@Log
+public class LoggingApplication implements CommandLineRunner {
+
+
+  public static void main(String[] args) {
+    SpringApplication.run(LoggingApplication.class, args);
+  }
+
+
+  @Override
+  public void run(String... args) throws Exception {
+    log.info("Start applicatoin");
+
+    log.info("End program");
+  }
+}
