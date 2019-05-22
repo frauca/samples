79dc82a3fffe6d001dc7495451de7eec7fd9a5d2
roger frauca <roger.frauca@dxcfds.com>
2019-05-22 23:29:41 +0200

    clean code

--- java/clean_code/parallel/src/test/java/clean/code/paralel/ClassWithThreadingProblemTest.java
+++ java/clean_code/parallel/src/test/java/clean/code/paralel/ClassWithThreadingProblemTest.java
@@ -14,7 +14,7 @@ public class ClassWithThreadingProblemTest {
   @Test
   public void whenRun100Times_ThenAlwaysIncrent2() throws ExecutionException, InterruptedException {
     final ClassWithThreadingProblem counter = new ClassWithThreadingProblem();
-    for(int i=0;i<100;i++){
+    for(int i=0;i<1000;i++){
       int current = counter.nextId;
       Callable<Integer> increment = new Callable<Integer>() {
         public Integer call() throws Exception {
@@ -23,11 +23,13 @@ public class ClassWithThreadingProblemTest {
       };
       Future<Integer> c1 = Executors.newSingleThreadExecutor().submit(increment);
       Future<Integer> c2 = Executors.newSingleThreadExecutor().submit(increment);
+      Future<Integer> c3 = Executors.newSingleThreadExecutor().submit(increment);
       c1.get();
       c2.get();
-      if(current+2 != counter.nextId)
+      c3.get();
+      if(current+3 != counter.nextId)
         return;
     }
-    fail("Should get end before");
+    fail("It must fail. The class is not thread save");
   }
 }
