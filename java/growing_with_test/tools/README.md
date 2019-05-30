086dea1951a6bdeba6d7d5af3d723813993dfffc
roger frauca <roger.frauca@dxcfds.com>
2019-05-30 11:42:46 +0200

    Add JMockit

--- java/growing_with_test/tools/README.md
+++ java/growing_with_test/tools/README.md
@@ -2,4 +2,8 @@
 
 #Tools chapter
 
-I will follow the chapter but I will try to use the current version of the software
+I will follow the chapter but I will try to use the current version of the software.
+
+The book uses Jmock2. Looks like a litle bit outdate.
+
+I do not knwow if use mockit or mockito. [Baeldung choose Mockit](https://www.baeldung.com/mockito-vs-easymock-vs-jmockit), so I will do the same.
--- java/growing_with_test/tools/src/test/java/growing/test/tools/ListTest.java
+++ java/growing_with_test/tools/src/test/java/growing/test/tools/ListTest.java
@@ -1,16 +1,25 @@
 package growing.test.tools;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import mockit.Expectations;
+import mockit.Mocked;
+import mockit.Verifications;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ListTest {
 
   private List<String> list = new ArrayList<>();
+
 
   @BeforeEach
   void emptyList() {
@@ -23,12 +32,34 @@ class ListTest {
     assertEquals(1,list.size());
   }
 
-  @Test()
+  @Test
   void whenNullAdded_thenThrowNullPointer(){
     list.add("sample");
     assertThrows(IndexOutOfBoundsException.class,()->{
       list.get(3);
     });
+  }
 
+  @Test
+  void whenStringAdded_ContainsTheString(){
+    list.add("sample");
+    assertThat(list, hasItem("sample"));
+
+  }
+
+  @Test
+  void givenStreaOfList_whenFiltered_testIsCalled(@Mocked Predicate<String> filter) {
+    new Expectations(){{
+      filter.test(anyString); returns(true,false);
+    }};
+    list.add("sample");
+    list.add("toFilter");
+
+    List<String> result = list.stream().filter(filter).collect(Collectors.toList());
+    assertThat(result.size(),is(1));
+
+    new Verifications(){{
+      filter.test(anyString); times = 2;
+    }};
   }
 }
