7cc426cbf07bbf7b487fb388360b7b320e152f11
Roger Frauca <roger.frauca@gmail.com>
2019-06-11 23:52:48 +0200

    Chapter 10. Non working test

---
+++ java/growing_with_test/auction/src/test/java/test/endtoend/auctionsniper/AuctionSniperEndToEndTest.java
@@ -0,0 +1,30 @@
+package test.endtoend.auctionsniper;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+public class AuctionSniperEndToEndTest {
+
+    private final FakeAuctionServer auction = new FakeAuctionServer("item-54321");
+    private final ApplicationRunner application = new ApplicationRunner();
+
+    @Test
+    public void sniperJoinsAuctionUntilAuctionCloses() throws Exception {
+        auction.startSellingItem();
+        application.startBiddingIn(auction);
+        auction.hasReceivedJoinRequestFromSniper();
+        auction.announceClosed();
+        application.showsSniperHasLostAuction();
+    }
+
+    // Additional cleanup
+    @AfterEach
+    public void stopAuction() {
+        auction.stop();
+    }
+
+    @AfterEach
+    public void stopApplication() {
+        application.stop();
+    }
+}
