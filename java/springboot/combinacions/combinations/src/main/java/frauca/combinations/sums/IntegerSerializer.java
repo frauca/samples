b8b0328f33b3c496f988a148420d7eb8551ff1af
frauca <roger.frauca@gmail.com>
2020-09-13 08:28:33 +0200

    adding samples

---
+++ java/springboot/combinacions/combinations/src/main/java/frauca/combinations/sums/IntegerSerializer.java
@@ -0,0 +1,2 @@
+package frauca.combinations.sums;public class IntegerSerializer {
+}
---
+++ java/springboot/combinacions/combinations/src/test/java/frauca/combinations/sums/IntegerSerializerTest.java
@@ -0,0 +1,6 @@
+package frauca.combinations.sums;
+
+import static org.junit.jupiter.api.Assertions.*;
+class IntegerSerializerTest {
+  
+}
