a4b20671d3e89f551e2dac5f32b18604b1cda73b
frauca <roger.frauca@gmail.com>
2022-10-29 10:49:38 +0200

    Working sample of application login on github

---
+++ java/springboot/oauth/combine-auth/src/main/java/frauca/oauth/CombineAuthApplication.java
@@ -0,0 +1,13 @@
+package frauca.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CombineAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CombineAuthApplication.class, args);
+	}
+
+}
---
+++ java/springboot/oauth/combine-auth/src/test/java/frauca/oauth/CombineAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package frauca.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CombineAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
