8a3d6801deff9fa6a9ba9db64e86a7703cebc860
frauca <roger.frauca@gmail.com>
2022-10-29 10:49:52 +0200

    commit metrics sample

--- java/springboot/metrics/metrics/src/main/java/frauca/metrics/AppConfiguration.java
+++ java/springboot/metrics/metrics/src/main/java/frauca/metrics/AppConfiguration.java
@@ -1,6 +1,5 @@
 package frauca.metrics;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
--- java/springboot/metrics/metrics/src/main/java/frauca/metrics/person/PersonController.java
+++ java/springboot/metrics/metrics/src/main/java/frauca/metrics/person/PersonController.java
@@ -1,20 +1,55 @@
 package frauca.metrics.person;
 
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController()
 @RequestMapping("/persons")
 public class PersonController {
 
-    private List<Person> db = List.of(new Person("Jaume"), new Person("Manel"));
+    private final List<Person> db;
+    private final Counter listedTimes;
+    private final Timer elapsed;
+
+    public PersonController(MeterRegistry registry){
+        listedTimes = registry.counter("persons_requests");
+        db = registry.gauge("persons", new ArrayList<Person>(), ArrayList::size);
+        elapsed = registry.timer("persons_elapsed");
+    }
 
     @GetMapping
     public Flux<Person> list(){
-        return Flux.fromIterable(db);
+        return elapsed.record(()->{
+            listedTimes.increment();
+            gauge();
+            return Flux.fromIterable(db);
+        });
+    }
+
+    private void gauge(){
+        final var count = listedTimes.count();
+        if(count %2==0){
+            db.add(new Person("Jaume"));
+        } else if (count % 3 == 0){
+            db.add(new Person("Manel"));
+        } else if (count % 5 == 0){
+            db.add(new Person("Pep"));
+        }else if (count % 7 == 0){
+            db.add(new Person("Teo"));
+        } else if (count % 11 == 0){
+            db.add(new Person("Zoe"));
+        } else {
+            db.clear();
+        }
     }
 }
