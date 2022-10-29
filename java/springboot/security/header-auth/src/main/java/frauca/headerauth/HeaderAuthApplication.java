f3b9a2bb024d02fa6b067ee4674fa6999891be68
frauca <roger.frauca@gmail.com>
2022-10-29 10:46:31 +0200

    preparing for rebase

---
+++ java/springboot/security/header-auth/src/main/java/frauca/headerauth/HeaderAuthApplication.java
@@ -0,0 +1,13 @@
+package frauca.headerauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeaderAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeaderAuthApplication.class, args);
+	}
+
+}
