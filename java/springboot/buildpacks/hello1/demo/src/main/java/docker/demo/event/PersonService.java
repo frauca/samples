aa06528c2d2119b1da63c917d661a0f3e5db3c2d
frauca <roger.frauca@gmail.com>
2023-05-25 08:09:21 +0200

    adding logs

--- java/springboot/buildpacks/hello1/demo/src/main/java/docker/demo/event/PersonService.java
+++ java/springboot/buildpacks/hello1/demo/src/main/java/docker/demo/event/PersonService.java
@@ -1,7 +1,11 @@
 package docker.demo.event;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class PersonService {
 
+  private final Logger log = LoggerFactory.getLogger(PersonService.class);
   private final PersonRepository repository;
 
   public PersonService(PersonRepository repository) {
@@ -10,12 +14,15 @@ public class PersonService {
 
   public Person create(Person person){
     if(person.id()!=null) {
+      log.info("Trying to make a person with id {}. We are getting for it", person.id());
       return repository.findById(person.id()).orElseThrow();
     }
+    log.info("Creating person with email {}",person.mail());
     return repository.save(person);
   }
 
   public Iterable<Person> all(){
+    log.info("Get all persons");
     return repository.findAll();
   }
 }
