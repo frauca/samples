cd5ba431fe343fae2f47aa7c3b956d60eddf8a78
frauca <roger.frauca@gmail.com>
2023-08-01 16:46:51 +0200

    switching to he fast api project again

--- python/mock/hello/tests/unit/conftest.py
+++ python/mock/hello/tests/unit/conftest.py
@@ -2,6 +2,7 @@ from mock import Mock
 import pytest
 from db.person import PersonDao
 from db.database import DB
+from person.service import PersonService
 
 @pytest.fixture()
 def empty_db()->DB:
@@ -14,3 +15,7 @@ def mocked_db() -> DB:
 @pytest.fixture()
 def person_dao(mocked_db:DB) -> PersonDao:
     return PersonDao(mocked_db)
+
+@pytest.fixture()
+def person_service(person_dao) -> PersonService:
+    return PersonService(person_dao)
